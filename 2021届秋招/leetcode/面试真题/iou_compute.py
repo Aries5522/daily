c19fad0022b30cf232515a1f36b361a3ee27b512
aries <Aries5522>
2020-08-20 15:37:40 +0800

    updata

--- 2021届秋招/leetcode/面试真题/iou_compute.py
+++ 2021届秋招/leetcode/面试真题/iou_compute.py
@@ -63,6 +63,36 @@ def NMS(list_reference, threshold):
     return res
 
 
+import numpy as np
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+# ---自己按照公式实现
+def auc_calculate(labels, preds, n_bins=100):
+    postive_len = sum(labels)
+    negative_len = len(labels) - postive_len
+    total_case = postive_len * negative_len
+    pos_histogram = [0 for _ in range(n_bins)]
+    neg_histogram = [0 for _ in range(n_bins)]
+    bin_width = 1.0 / n_bins
+    for i in range(len(labels)):
+        nth_bin = int(preds[i] / bin_width)
+        if labels[i] == 1:
+            pos_histogram[nth_bin] += 1
+        else:
+            neg_histogram[nth_bin] += 1
+    accumulated_neg = 0
+    satisfied_pair = 0
+    for i in range(n_bins):
+        satisfied_pair += (pos_histogram[i] * accumulated_neg + pos_histogram[i] * neg_histogram[i] * 0.5)
+        accumulated_neg += neg_histogram[i]
+
+    return satisfied_pair / float(total_case)
+
+
+
+
+
 if __name__ == '__main__':
     rec1 = (1, 1, 3, 3)
     rec2 = (2, 2, 4, 4)
@@ -72,3 +102,10 @@ if __name__ == '__main__':
 
     list_test = [(1, 1, 3, 3, 0.99), (1, 1, 3, 3, 0.8), (2, 2, 4, 4, 0.3)]
     print(NMS(list_test, threshold=0.6))
+
+    y = np.array([1, 0, 0, 0, 1, 0, 1, 0, ])
+    pred = np.array([0.9, 0.8, 0.3, 0.1, 0.4, 0.9, 0.66, 0.7])
+
+    fpr, tpr, thresholds = roc_curve(y, pred, pos_label=1)
+    print("-----sklearn:", auc(fpr, tpr))
+    print("-----py脚本:", auc_calculate(y, pred))
